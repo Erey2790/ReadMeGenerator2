d2a48ef8a452734d8356ff2fe169817f48907458
ElmerPC <erey2790@gmail.com>
2022-01-24 23:56:46 -0600

    added render license badge

--- Develop/output/README.md
+++ Develop/output/README.md
@@ -1,7 +1,10 @@
-# ReadMe generator
+# ldaskjf
   ## Description
-  ita asldkjfalsdjf  ldk jfalkjsd fla sdfl jdlf j;alskdj f;laksd jflaksd jf;lak jdflka jd;lfk ja;sdlk f;lksd jf;aklsd jf; laksjdf;oijeorfas ;ldkfjals jf;ajdf;ajk
+  alskdfj
 
+  
+    !MIT(https://img.shields.io/badge/License-MIT-blue)
+    
   ## Table of Contents
   * [Installation](Installation)
   * [Usage](Usage)
@@ -10,21 +13,21 @@
   * [Questions](Questions)
   
   ## Installation
-  a;lsdfj;alksdjf;alksdjf;lksdjf;aklsdfj;aldkfja;sdlkfj  ;lkajsd;f sd ;fa sdfkj al;sdf j;alskd jfalk sdfal k
+  asd;fl
 
   ## Usage
-  a;lsdjfaijeo;rfjasel;krfja  jhas;dlfja;ld
+  a;lsdjf
 
   ## Credits
-  ;aslediru;alksejrflajedrlkaje;irj;l
+  a;sldf
 
   ## License
-  APACHE 2.0
+  MIT
 
   ## Questions
   Please contact me using the following links:
   [Github](https://github.com/erey2790)
-  [Email: erey2790@gamil.com](mailto:erey2790@gamil.com)
+  [Email: erey2790@gmail.com](mailto:erey2790@gmail.com)
 
 
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,26 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'no license') {
+    return `
+    !${license}(https://img.shields.io/badge/License-${license}-blue)
+    `;
+  } else {
+    return '';
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license !== 'no license link') {
+    return `
+    
+    `;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -16,6 +32,7 @@ function generateMarkdown(data) {
   ## Description
   ${data.description}
 
+  ${renderLicenseBadge(data.license)}
   ## Table of Contents
   * [Installation](Installation)
   * [Usage](Usage)
